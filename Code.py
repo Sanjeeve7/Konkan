3d24965ebc5b3145e4475a4b9206c7dc42d58a24
Sanjeeve R <rsanjeevecbe@gmail.com>
2022-05-13 16:53:43 +0530

    Partial Code

--- Code.py
+++ Code.py
@@ -1,6 +1,116 @@
 import RPi.GPIO as io
-io.setmode(GPIO.BOARD)
+from smbus2 import SMBus 
 
-mux_Select_pins = {27,29,31,33}
-i2c_mux_rst_pin = 7;
+io.setwarnings(False)
 
+io.setmode(io.BCM)
+
+def init_pins( mux_Select_pins, i2c_mux_rst_pin, mod_sel_pins, output_pins ):
+
+    for i in mux_Select_pins:
+        io.setup(i,io.OUT)
+
+    for i in mod_sel_pins:
+        io.setup(i,io.OUT)
+
+    for i in output_pins:
+        io.setup(i,io.OUT)
+    return 0
+
+def select_module(mod_num):
+    write_data = [0,0,0]
+    if mod_num == 1:
+        write_data = [0,0,0]
+    elif mod_num == 2:
+        write_data = [0,0,1]
+    elif mod_num == 3:
+        write_data = [0,1,0]
+    elif mod_num == 4:
+        write_data = [0,1,1]
+    elif mod_num == 5:
+        write_data = [1,0,0]
+    elif mod_num == 6:
+        write_data = [1,0,1]
+    elif mod_num == 7:
+        write_data = [1,1,0]
+    elif mod_num == 8:
+        write_data = [1,1,1]
+
+    for i in range (0,3):
+        print(type(mod_sel_pins[i]))
+        io.output(mod_sel_pins[i],write_data[i])
+    
+    bus.write_byte(0x70, 1 << (mod_num-1))
+
+    return 0
+
+def select_mux(mux_num):
+
+    A0 = 0X40
+    A1 = 0X41
+    A2 = 0X42
+
+    if mux_num == 1:
+        bus.write_byte( 0x48, A0 )
+    elif mux_num == 2:
+        bus.write_byte( 0x48, A1 )
+    elif mux_num == 3:
+        bus.write_byte ( 0x48, A2 )
+
+    return 0
+
+def select_sensor(sensor_num):
+    write_data = [0,0,0,0]
+    if sensor_num == 0:
+        write_data = [0,0,0,0]
+    elif sensor_num == 1:
+        write_data = [0,0,0,1]
+    elif sensor_num == 2:
+        write_data = [0,0,1,0]
+    elif sensor_num == 3:
+        write_data = [0,0,1,1]
+    elif sensor_num == 4:
+        write_data = [0,1,0,0]
+    elif sensor_num == 5:
+        write_data = [0,1,0,1]
+    elif sensor_num == 6:
+        write_data = [0,1,1,0]
+    elif sensor_num == 7:
+        write_data = [0,1,1,1]
+    elif sensor_num == 8:
+        write_data = [1,0,0,0]
+    elif sensor_num == 9:
+        write_data = [1,0,0,1]
+    elif sensor_num == 10:
+        write_data = [1,0,1,0]
+    elif sensor_num == 11:
+        write_data = [1,0,1,1]
+    elif sensor_num == 12:
+        write_data = [1,1,0,0]
+    elif sensor_num == 13:
+        write_data = [1,1,0,1]
+    elif sensor_num == 14:
+        write_data = [1,1,1,0]
+    elif sensor_num == 15:
+        write_data = [1,1,1,1]
+    
+    for i in range (0,4):
+        io.OUTPUT(mod_sel_pins[i],write_data[i])
+    
+    return 0
+
+def read_sensor_data():
+    value = bus.read_byte(0x48)
+    return value
+
+mux_Select_pins = [13,6,5,0]
+i2c_mux_rst_pin = 4
+mod_sel_pins = {22,27,17}
+output_pins = {14,15,18,23}
+
+init_pins( mux_Select_pins, i2c_mux_rst_pin, mod_sel_pins, output_pins ) #Initialize pins
+bus = SMBus(1) #Initialize I2C
+select_module(1) #Select the module
+select_sensor() #Select from which sensor we need input
+select_mux() #Select from which mux we need the input using ADC Input Selection
+read_sensor_data() #Read data from selected sensor
